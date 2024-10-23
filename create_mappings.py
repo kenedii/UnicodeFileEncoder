4e2f42edf799e8d0c77d1a64763ff11e63a0911d
kenedii <55466055+kenedii@users.noreply.github.com>
2024-10-22 21:37:39 -0400

    Mappings save to proper folder
    - mappings now save to proper folder
    - encode function automatically creates decode mappings after it.
    - random mappings still need to be implemented

--- create_mappings.py
+++ create_mappings.py
@@ -1,8 +1,9 @@
 import json
+import os
 
 # Creates a dictionary of pairs that map 4 hex nibbles (2 bytes) to a single unicode character
 def create_encode_dict(random=False, filename='hex_to_unicode'):
-    if random==False:
+    if random==False: #default mappings
         # Create a dictionary to hold the mappings
         hex_to_unicode = {}
 
@@ -18,14 +19,16 @@ def create_encode_dict(random=False, filename='hex_to_unicode'):
             hex_to_unicode[hex_key] = unicode_char
 
         # Save the dictionary to a text file
-        with open(f'{filename}.json', 'w', encoding='utf-8') as f:
+        with open(os.path.join("mappings/default",f'{filename}.json'), 'w', encoding='utf-8') as f:
             json.dump(hex_to_unicode, f, ensure_ascii=False, indent=4)
         print(f"Mapping saved to '{filename}.json'")
+        create_decode_dict()
+    #else: # If we are creating random mappings.
 
-def create_decode_dict(filename='hex_to_unicode'):
+def create_decode_dict(filepath=os.path.join("mappings/default",f'hex_to_unicode.json')):
     # Read the JSON file with the appropriate encoding
     try:
-        with open('{filename}.json', 'r', encoding='utf-8') as file:
+        with open(f'{filepath}.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
@@ -34,12 +37,12 @@ def create_decode_dict(filename='hex_to_unicode'):
     # Swap keys and values
     swapped_data = {key: value for value, key in data.items()}
 
-    if filename == 'hex_to_unicode':
-        decode_table_filename = 'unicode_to_hex'
+    if os.path.splitext(os.path.basename(filepath))[0] == 'hex_to_unicode':
+        decode_table_filename = os.path.join("mappings/default", 'unicode_to_hex.json')
     else:
-        decode_table_filename = f'DECODE_{filename}'
+        decode_table_filename = os.path.join("mappings", f'DECODE_{os.path.splitext(os.path.basename(filepath))[0]}.json')
     # Write the swapped data to a new JSON file with UTF-8 encoding
     with open(f'{decode_table_filename}.json', 'w', encoding='utf-8') as file:
         json.dump(swapped_data, file, indent=4, ensure_ascii=False)
 
-    print(f"{filename} Decode table successfully created under {decode_table_filename}.json")
+    print(f"{os.path.splitext(os.path.basename(filepath))[0]} Decode table successfully created under {decode_table_filename}.json")
