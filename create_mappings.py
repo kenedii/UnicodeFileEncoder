2c895105e8ed06393bfc4fdd28bfa7520b7de807
kenedii <55466055+kenedii@users.noreply.github.com>
2024-10-23 00:05:42 -0400

    Mappings were not being done correctly
    hex 0000-001A were being mapped starting from U0020, but hex 0020 would start mapping to U0020 again.
    Fixed by ensuring the mapping starts at 0000:U0020 and is continuous

--- create_mappings.py
+++ create_mappings.py
@@ -14,11 +14,11 @@ def create_encode_dict(random=False, filename='hex_to_unicode'):
         # Generate mappings for 0000 to FFFF
         for i in range(0x10000):  # 0x10000 is 65536 in decimal, covering 0000 to FFFF
             hex_key = f"{i:04X}"  # Format as 4-digit hex
-            if 0xD800 <= i <= 0xDFFF:
+            if 0xD800 <= i+ 0x0020 <= 0xDFFF:
                 continue  # Skip surrogate characters
 
-            # Map hex bytes to Unicode characters starting from U+0020
-            unicode_char = chr(i + 0x0020) if i < 0x0020 else chr(i)  # Shift for 0000 to 001F
+            # Map hex bytes to Unicode characters starting from U+0020, but hex '0000' starts at ' '
+            unicode_char = chr(i + 0x0020)  # Map and 0000 to U+0020
 
             hex_to_unicode[hex_key] = unicode_char
 
@@ -26,7 +26,10 @@ def create_encode_dict(random=False, filename='hex_to_unicode'):
         with open(os.path.join(output_dir, f'{filename}.json'), 'w', encoding='utf-8') as f:
             json.dump(hex_to_unicode, f, ensure_ascii=False, indent=4)
         print(f"Mapping saved to '{filename}.json'")
+
+        # Also create the corresponding decode dictionary
         create_decode_dict()
+    #else: # If we are creating random mappings.
 
 def create_decode_dict(filepath=os.path.join("mappings", "default", 'hex_to_unicode.json')):
     # Read the JSON file with the appropriate encoding
@@ -35,10 +38,9 @@ def create_decode_dict(filepath=os.path.join("mappings", "default", 'hex_to_unic
             data = json.load(file)
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
-        exit()
 
     # Swap keys and values
-    swapped_data = {value: key for key, value in data.items()}
+    swapped_data = {key: value for value, key in data.items()}
 
     if os.path.splitext(os.path.basename(filepath))[0] == 'hex_to_unicode':
         decode_table_filename = os.path.join("mappings", "default", 'unicode_to_hex.json')
@@ -49,5 +51,4 @@ def create_decode_dict(filepath=os.path.join("mappings", "default", 'hex_to_unic
     with open(decode_table_filename, 'w', encoding='utf-8') as file:
         json.dump(swapped_data, file, indent=4, ensure_ascii=False)
 
-    print(f"{os.path.splitext(os.path.basename(filepath))[0]} Decode table successfully created under {decode_table_filename}")
-
+    print(f"Decode table successfully created at {decode_table_filename}")
